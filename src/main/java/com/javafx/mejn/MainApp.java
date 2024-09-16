2f2277897b3bbeb4dc2f7068a7838e64b7c54311
Joep Rottinghuis <jrottinghuis@users.noreply.github.com>
2024-09-15 21:06:03 -0700

    Added initial Hello World UI

---
+++ src/main/java/com/javafx/mejn/MainApp.java
@@ -0,0 +1,41 @@
+package com.javafx.mejn;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    /**
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
