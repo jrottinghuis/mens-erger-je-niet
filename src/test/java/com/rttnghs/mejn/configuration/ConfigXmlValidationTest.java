e513ccd41eb80a816970fb2c65991e795c4a3d66
Joep Rottinghuis <jrottinghuis@gmail.com>
2026-04-24 09:54:38 -0700

    Prevent XML External Entity (XXE) attacks by blocking external resources during XML processing.

--- src/test/java/com/rttnghs/mejn/configuration/ConfigXmlValidationTest.java
+++ src/test/java/com/rttnghs/mejn/configuration/ConfigXmlValidationTest.java
@@ -42,6 +42,8 @@ class ConfigXmlValidationTest {
 
     private void validateXmlAgainstSchema(String xmlFile, String schemaFile) throws SAXException, IOException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
         Schema schema = factory.newSchema(new File(schemaFile));
         Validator validator = schema.newValidator();
         validator.validate(new StreamSource(new File(xmlFile)));
